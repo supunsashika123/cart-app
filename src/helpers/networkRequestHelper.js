a846ffbdd2cd39d68160540e55708ddd4827a1aa
supunsashika123 <supun.eternal@gmail.com>
2021-05-12 22:49:45 +0530

    Network request hepler added with fetch

---
+++ src/helpers/networkRequestHelper.js
@@ -0,0 +1,108 @@
+import { BASE_API_URL } from "../config";
+
+
+export async function httpGenericGetRequest(params = null) {
+  console.log(params.url);
+  let token = ''
+  return fetch(params.url, {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + token,
+    },
+  })
+    .then(async (response) => {
+      if (response.status === 403) {
+      }
+
+      return response.json();
+    })
+    .then(async (responseJson) => {
+      return responseJson;
+    })
+    .catch((error) => {
+      console.error(error);
+      return error;
+    });
+}
+
+export async function httpGetRequest(params = null) {
+  console.log(BASE_API_URL + params.url);
+  let token = ''
+
+  return fetch(BASE_API_URL + params.url, {
+    method: 'GET',
+    headers: {
+      Authorization: 'Bearer ' + token,
+    },
+  })
+    .then(async (response) => {
+      if (response.status === 403) {
+      }
+
+      return response.json();
+    })
+    .then(async (responseJson) => {
+      return responseJson;
+    })
+    .catch((error) => {
+      console.error(error);
+      return error;
+    });
+}
+
+export async function httpPostRequest(params) {
+  console.log(BASE_API_URL + params.url);
+  let token = ''
+
+  return fetch(BASE_API_URL + params.url, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + token,
+    },
+    body: JSON.stringify(params.body),
+  })
+    .then(async (response) => {
+      if (response.status === 403) {
+      }
+
+      return response.json();
+    })
+    .then(async (responseJson) => {
+      return responseJson;
+    })
+    .catch((error) => {
+      console.error(error);
+      return error;
+    });
+}
+
+export async function httpPutRequest(params) {
+  console.log(BASE_API_URL + params.url);
+  let token = ''
+
+  return fetch(BASE_API_URL + params.url, {
+    method: 'PUT',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+      Authorization: 'Bearer ' + token,
+    },
+    body: JSON.stringify(params.body),
+  })
+    .then(async (response) => {
+      if (response.status === 403) {
+      }
+
+      return response.json();
+    })
+    .then(async (responseJson) => {
+      return responseJson;
+    })
+    .catch((error) => {
+      console.error(error);
+      return error;
+    });
+}
+
